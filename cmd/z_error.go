b61e0c07b5cef91b563de182c3d1616e65eab54f
Hyori 효리 <hyorigo@icloud.com>
2024-05-18 22:07:18 +0800

    Reset

--- cmd/z_error.go
+++
@@ -1,9 +0,0 @@
-package cmd
-
-import "errors"
-
-// Errors for commands and subcommands
-var (
-	errNotImplemented    = errors.New("not implemented")
-	errConfigKeyNotFound = errors.New("config key not found")
-)
