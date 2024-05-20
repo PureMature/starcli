aab3215fd11ccf96405504e886124fa8e3e68b05
Hyori 효리 <hyorigo@icloud.com>
2024-05-20 19:19:28 +0800

    build it

---
+++ cli/action.go
@@ -0,0 +1,24 @@
+package cli
+
+import flag "github.com/spf13/pflag"
+
+func runWebServer(args *Args) error {
+	return nil
+}
+
+func runDirectCode(args *Args) error {
+	return nil
+}
+
+func runREPL(args *Args) error {
+	return nil
+}
+
+func runScriptFile(args *Args) error {
+	return nil
+}
+
+func showHelp(args *Args) error {
+	flag.Usage()
+	return nil
+}
--- cli/arg.go
+++ cli/arg.go
@@ -72,24 +72,3 @@ func Process(args *Args) int {
 	}
 	return 0
 }
-
-func runWebServer(args *Args) error {
-	return nil
-}
-
-func runDirectCode(args *Args) error {
-	return nil
-}
-
-func runREPL(args *Args) error {
-	return nil
-}
-
-func runScriptFile(args *Args) error {
-	return nil
-}
-
-func showHelp(args *Args) error {
-	flag.Usage()
-	return nil
-}
