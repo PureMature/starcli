f730b646a74b076fc4015c08e2f2f98404397b5e
Hyori 효리 <hyorigo@icloud.com>
2024-05-21 23:27:05 +0800

    for output mo

--- cli/box.go
+++ cli/box.go
@@ -3,6 +3,8 @@ package cli
 import (
 	"fmt"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/1set/gut/ystring"
 	"github.com/1set/starbox"
@@ -10,6 +12,7 @@ import (
 	"github.com/PureMature/starcli/module/sys"
 	"github.com/PureMature/starcli/util"
 	"go.starlark.net/starlark"
+	"go.uber.org/atomic"
 )
 
 // BuildBox creates a new Starbox with the given name, include path, and modules.
@@ -20,9 +23,7 @@ func BuildBox(name, inclPath string, modules, args []string, printer string) *st
 	if ystring.IsNotBlank(inclPath) {
 		box.SetFS(os.DirFS(inclPath))
 	}
-	box.SetPrintFunc(func(thread *starlark.Thread, msg string) {
-		fmt.Println(msg)
-	})
+	box.SetPrintFunc(getPrinterFunc(name, printer))
 	// add default modules
 	box.AddModuleLoader(sys.ModuleName, sys.NewModule(args))
 	return box
@@ -40,5 +41,32 @@ func genInspectCond(inspect bool) starbox.InspectCondFunc {
 	}
 	return func(starlet.StringAnyMap, error) bool {
 		return false
+	}
+}
+
+// getPrinterFunc returns a function to print output based on the given printer name.
+func getPrinterFunc(name, printer string) func(*starlark.Thread, string) {
+	switch strings.ToLower(strings.TrimSpace(printer)) {
+	case "none", "nil", "no":
+		return func(thread *starlark.Thread, msg string) {}
+	case "stdout":
+		return func(thread *starlark.Thread, msg string) {
+			fmt.Println(msg)
+		}
+	case "stderr":
+		return func(thread *starlark.Thread, msg string) {
+			fmt.Fprintln(os.Stderr, msg)
+		}
+	case "basic":
+		return nil
+	case "lineno", "linenum":
+		cnt := atomic.NewInt64(0)
+		return func(thread *starlark.Thread, msg string) {
+			//prefix := fmt.Sprintf("%04d [⭐|%s](%s)", cnt.Inc(), name, time.Now().UTC().Format(`15:04:05.000`))
+			prefix := fmt.Sprintf("[%04d](%s)", cnt.Inc(), time.Now().UTC().Format(`15:04:05.000`))
+			fmt.Fprintln(os.Stderr, prefix, msg)
+		}
+	default:
+		return nil
 	}
 }
